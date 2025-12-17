c9e2698b5e2a330c355e8902025ed0a70962e6a4
371_Methat <mathelos8989@gmail.com>
2025-12-17 16:32:58 +0700

    vite

--- vite.config.js
+++ vite.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
         comments: false,
       },
       server: {
-        allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'webcomcamp24.com', 'webcomcamp24.com:5173','comcamp.csmju.com'],
+        allowedHosts: ['localhost', '127.0.0.1', '0.0.0.0', 'webcomcamp24.com', 'webcomcamp24.com:5173','comcamp.csmju.com','comcamp.csmju.com:5173'],
       },
     },
   },
